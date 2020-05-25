a6a51a462ac0c4b4edc7210ed54e27432b9937e7
Alexander Treib <alextreib94@gmail.com>
2020-05-25 21:55:14 +0200

    modify loop -> try

--- cron_update.py
+++ cron_update.py
@@ -13,20 +13,23 @@ date_yesterday_format="{}-{}-{}".format(date_yesterday.strftime('%Y'), date_yest
 Contracts_csv_url = 'http://stockvoting.net/openShare/ContractActionKO_creation.csv'
 
 with requests.Session() as s:
-    download = s.get(Contracts_csv_url)
-    decoded_content = download.content.decode('utf-8')
+    try:
+        download = s.get(Contracts_csv_url)
+        decoded_content = download.content.decode('utf-8')
 
-    split_content = csv.reader(decoded_content.splitlines(), delimiter=',')
-    ContractsList = list(split_content)
-    for contract_array in ContractsList:
-        contract_address=contract_array[0]
-        underlying=contract_array[1]
+        split_content = csv.reader(decoded_content.splitlines(), delimiter=',')
+        ContractsList = list(split_content)
+        for contract_array in ContractsList:
+            contract_address=contract_array[0]
+            underlying=contract_array[1]
 
-        # date
-        eod_array=sts.batch_request_eod_prices([underlying], date=date_yesterday_format)
-        closing_price=eod_array['close'].values[0]
-        low_price=eod_array['low'].values[0]
+            # date
+            eod_array=sts.batch_request_eod_prices([underlying], date=date_yesterday_format)
+            closing_price=eod_array['close'].values[0]
+            low_price=eod_array['low'].values[0]
 
-        call_script_cmd="geth --rpcapi='db,eth,net,web3,personal' --rpc --exec \"loadScript('geth_script.js'); main('{}',{},{})\" attach http://127.0.0.1:8545".format(contract_address, low_price,closing_price)
-        print(call_script_cmd)
-        subprocess.call(call_script_cmd, shell=True)
+            call_script_cmd="geth --rpcapi='db,eth,net,web3,personal' --rpc --exec \"loadScript('geth_script.js'); main('{}',{},{})\" attach http://127.0.0.1:8545".format(contract_address, low_price,closing_price)
+            print(call_script_cmd)
+            subprocess.call(call_script_cmd, shell=True)
+    except:
+        pass
